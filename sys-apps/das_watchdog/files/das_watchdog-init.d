3ccef4bb46e8adff06237679d64dc7ec31e5a25b
Jonathan Lovelace <kingjon3377@gmail.com>
2015-06-26 19:26:44 -0400

    Don't compress the trivially-small init script for das_watchdog.

---
+++ sys-apps/das_watchdog/files/das_watchdog-init.d
@@ -0,0 +1,18 @@
+#!/sbin/runscript
+# Copyright 1999-2006 Gentoo Foundation
+# Distributed under the terms of the GNU General Public License v2
+# $Header: $
+
+
+NAME="das_watchdog"
+EXEC="/usr/sbin/${NAME}"
+
+start() {
+	ebegin "Starting $NAME"
+	start-stop-daemon --start --quiet --background --exec $EXEC
+}
+
+stop() {
+	ebegin "Stopping $NAME"
+	start-stop-daemon --stop --quiet --oknodo --exec $EXEC
+}
