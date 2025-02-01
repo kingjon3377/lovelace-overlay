kingjon3377/lovelace-overlay

commit 4644ad639af89b7c22af52b27feb32f553fc68df

*/*: Remove use of java-ant-2.eclass
Which has been suddenly removed from ::gentoo, without any documentation of what to do
instead. None of these changes have been tested.

app-misc/mapcraft/Manifest
@@ -1,5 +1,5 @@
 AUX incompatible-type.patch 707 BLAKE2B ed54793876bb9a13f8771d3eda49edf482fb84605515e31a161c51d0667e3987455642754e42ffe8ce988438753a274fc1a35013c650efc3fd8ae009a22906be SHA512 8b759a08c623e2ff885bb8bfc58281e8bbcf4a8a576ddda3efe71a04f013d46e5a7f2e77ce28ec6c3e3e826450635cb289d8c29f619c2a7e5d141338b2d211ae
 AUX internal-api.patch 781 BLAKE2B 8823b83ed42db84a628488f42529db9a253f9867c5bb171e209048f8d423d2201941204f976060f3f1e89aa31b0fc5e1b5a6a9504bbcde0af0352fc6138fe1ea SHA512 378bf065f198ea642f6f8254bcb02f70a2722732ca815870888f82caf0abc4efd1ad695c99ebc7ca7d4f7389aa393c191d851f762632c14654810f2d8b4e0b8e
 DIST mapcraft-0.3b.tgz 1838057 BLAKE2B f914cff40fa4182c5360b5f001784b1274ee198bf51dc1ae53dfd78eed21dbc7cb3b58548f2f5baf38e60c7614d3d962cb3d4a637ff9c6ae098bf8c778b54368 SHA512 edafa75b84c46b5384add84bef5ece62b1a98f234183cec2073919821b7d96c458c99027bef8b22ccb76ebd35d74ccd5f3d0367ac1ae8844968a6e62723e7eb8
-EBUILD mapcraft-0.3b.ebuild 1162 BLAKE2B fa9045d8d305c25f5741bb7590e0549b230a2485d045f42a7a5bf8a9cfbe0f056fd13b95b88c2116c5f1f5eae71c06f9017e1e04a338fcace36131c24dcb9dfa SHA512 f515fa6b2c9a36a2a682e46a383a48a9973b827da190b170858fd80e3f26dc55365978226e6e0a4db0bf441954c3891cda312b2ff866bef183c4cf43ecd66661
+EBUILD mapcraft-0.3b.ebuild 1095 BLAKE2B 51c34f5e676315256cc43e88fe827848ee005bcfd5772f567bbab0c236a0a9c4a977feef454c06ef82ddb20ce58c7368646bb4a5a7645c0ba91163f38465fa3c SHA512 5df3eb1c27aa35cf22aa84f093a47b029bf6407d3efef43b143bf4d618965338464713e242cb79e2afdb1cfd9463c86d49bdfda143dad5b60dd88ad0b1891f96
 MISC metadata.xml 214 BLAKE2B fc4a40d42b9ac5bc66bdcc886771d647cb3b8c88ff4e1d2f51bf40e261e9b9b8061b5c7527160007e63447bbed8a0a190b75cdd76a18341de5281ed0e4a1dbf0 SHA512 1b71cae306f6cbf69abe3051974d05050da9470c2b3b3655ebd1dd249091590fe10d6ca2df8f0e7c772671b57aa36c836cb9d86af069c250b43b8d1feea3e210

app-misc/mapcraft/mapcraft-0.3b.ebuild
@@ -5,7 +5,7 @@ EAPI=7
 
 EANT_GENTOO_CLASSPATH="xerces-2 xalan junit"
 
-inherit java-pkg-2 java-ant-2
+inherit java-pkg-2
 
 DESCRIPTION="Java based map editor for roleplaying games"
 HOMEPAGE="https://mapcraft.glendale.org.uk/"
@@ -27,11 +27,6 @@ PATCHES=(
 	"${FILESDIR}/internal-api.patch"
 	"${FILESDIR}/incompatible-type.patch"
 )
-
-src_prepare() {
-	java-ant_rewrite-classpath
-	default
-}
 
 src_compile() {
 	EANT_GENTOO_CLASSPATH="xerces-2 xalan junit" eant

app-text/writer2latex/Manifest
@@ -2,6 +2,6 @@ AUX XDocumentPropertiesSupplier-writer2xhtml.patch 2805 BLAKE2B 6e26e9285ee48f87
 AUX XDocumentPropertiesSupplier.patch 1594 BLAKE2B c162ad344db4016c0436186f14eee66652dc70a613f52bc89d384f5449934ab0ad170801ebb6bca43187e2f694f5608157201585838dac2b7c83f7a4ff07e6c6 SHA512 8cd7b17d0014a9ab25ce1fe7ba8f09071d4f8c5d5a09e0d9a49cbe6cd7452807423cfae8e008d01c142e14bb6148a8701aa31bd1ab4ab02169d20f0a0e2167b6
 DIST writer2latex-code-r336-tags-1.6.zip 1283824 BLAKE2B f47a4b0f8a56cdd5587de0dd1b2fc69e86eb631948e6fd5c973a99d36e28c95583c53726e395cbee0a0226408504216d3f687a70724e87297e1ccef0f9d9b03f SHA512 d923214575bb05e3e843130c48e2689c1d1b10ead10e1dfb53306ebd0bc76710ceb5d28171d3bea263efbdd17bd290fb8c78beff48e332135d0a7d95b7f9c4c3
 DIST writer2latex-code-r385-tags-1.6.1.zip 1293858 BLAKE2B 4ff4ab6e596a1e99bf3f2a691516695c2a85b3177d514f9b099ff819172738324ff6e6db8cfc1b34ca490cb733246152f6269eb7ef02077085d2401eb6e29ce2 SHA512 008cb35d88b1e03dc9bcc7b827181d94ddec78e99a026ab356d8af2432e11fd3ff921edf72035277da8b035e117962c01e9122ea4e7944803b922bfc882e3e2e
-EBUILD writer2latex-1.6.1.ebuild 2881 BLAKE2B f7ac584790bc5cc55a867c9211c6c73cfa846b1c30b0a2aab39a7e89a8ff114524e4494ff7f9f6cfd093e8494a065fdc96992a33f2f56871ec0a30a75b8143bc SHA512 3cf10427f557e6499a2c8ab7412604ecf282089baed60b9877a0a18a8974f043288dd565b108c482e86e1f067812704f9e23420004d0c0814bba7ae45e6f3dc1
-EBUILD writer2latex-1.6.ebuild 2718 BLAKE2B 089ccab197f02b1dde1b7d5618c24e5d4fbe833cbf19767ccc6aa999ecb02caa025b780d7f166e8ecf0eeeff7578761c8ca4f7d67dd38a8e0a66557f6c8cc98c SHA512 8adce1e324522db570ecac252a977bb9f998ce14f7033294ee2d73c4540d864dcce583278f4227374c1e623d9f90e83a628409eaf46c8d648ed91df3f98c5844
+EBUILD writer2latex-1.6.1.ebuild 2870 BLAKE2B a39dd309081f56cfbdf8e22d43cfd86f99a8ed13e90f6cdf7f5fe0e5efc9e1c1c26c9813a273a844cf5cde0a7b9eaed3c0d3f612a0a2d1a9c6c0219cffe84ce1 SHA512 ef16fd1844df67015c57437a3b0f18050c202634a4b1dca85793e4b31498a408ffeef345fee2c278a0bdf57f33d8a28e101185fe6645ec642c2bd6749a83e1fa
+EBUILD writer2latex-1.6.ebuild 2707 BLAKE2B 8b065ecd823e9ae142319be4b08adc98f009d9be1850dc9c2de9337d0280e8d000bf37c846d111c8319730a645eb9f436232572291d124ed4e90668ec51de3d5 SHA512 6e0a418668fcc6e0931f6f8350f60ab29d649c600d5fca178b2e2726bb5185617f5be849e9dd55fedcd8455e340b8ab67d65c381f07a53e3ed89702abf38c6f2
 MISC metadata.xml 140 BLAKE2B b079a1406c8131151cb09483d0bedc47488264a4fce263e413bafaf54ef7aef23d0173e17847908732fb42bf4e704a128050651f020f10e4cf2f44197874276d SHA512 7e8069b4a2eaf234ea437e95578e2fd6e007038bf98df9f508adfa3f5af0e746993074a91ada2517c520bfda84d193b33c7a41f184b78bf128aa51b5c0c76e40

dev-java/ant-compress/Manifest
@@ -1,3 +1,3 @@
 DIST apache-ant-compress-1.4-src.tar.gz 326993 BLAKE2B c0797bc9a1d99bb6f07f5646eb5ac11d63e58d72e0630ca404044b354e8ba24379c3443344b6bf2a458057ff43c19148ac42f82f0f8bc4e085abaeacbab2f260 SHA512 c6ed2dbfaf20e98c8c60e42ef7054b33ed64292af71f5d3d8bd4e10b4bb618127b04310ec09925a1dcc2cd84d769f71372ca00f9a8ccb1e4c909ac1f3575be22
-EBUILD ant-compress-1.4.ebuild 1456 BLAKE2B 98f3664d276f697064e75232b2bf15757567e30855a1fc9604cd91894a0e0a6d9cdeedd055fe864ab9ae4968ae0f0fdd491473908517e36192e3a8d34f888a98 SHA512 412b9be6f104466fa05bba9c282bddf83d61150eec680e362b99a70b2e08d891da8ab82e45d964aa30b709848f20ae87b6064978568f67073d5d6c704f5efb81
+EBUILD ant-compress-1.4.ebuild 1445 BLAKE2B e2c08fe045c0e9fd1aafbfbd531d29ab1ceea10d62f19dfe2ec59c36eb248c7a33cd975eb8f9e5514f0c3ef9ac49c52fe4d3e93a03925f81578c7ac19d0d64ca SHA512 a7fb070fe5f6a737dbd6963548b3059dcb267ae83cd154a6bfc3e9f4c314ec19f811f3f34200b51196145304f120b5b2a505b2d602515c2162ce53791a89bc78
 MISC metadata.xml 140 BLAKE2B b079a1406c8131151cb09483d0bedc47488264a4fce263e413bafaf54ef7aef23d0173e17847908732fb42bf4e704a128050651f020f10e4cf2f44197874276d SHA512 7e8069b4a2eaf234ea437e95578e2fd6e007038bf98df9f508adfa3f5af0e746993074a91ada2517c520bfda84d193b33c7a41f184b78bf128aa51b5c0c76e40

dev-java/diffxml/Manifest
@@ -1,3 +1,3 @@
 DIST diffxml-src-0.95B.tar.gz 408299 BLAKE2B ec7de1f9d5f1a67788090eb7a24b14414f0d2c3ea08309826f5e10765d0bf97d7221a5523d40ef23d10eadcc3eb320b4b18c1333c5b185aed7529bd506fb0e17 SHA512 9c5bf0177284e284f49a12a52fb054a3655dab3f7b3b050115506ea8c3cf1dd78006f1b106740818c31672de9337ad35aef152c3206df6c95d0e643e7ac5714c
-EBUILD diffxml-0.95_beta.ebuild 1107 BLAKE2B ef8e57cd3f0ad39f8fe06d6bb3ed65692b7b13b906b5a9e7417811c993bea1e209edbd05855eba4ea491a78b5af2577031b323827f7a6f917d2a60675ac62fc8 SHA512 a20165fca63f6d48d5ec3e71b92967d1e1c08c9cbbdb71bad4c0fd48d7b072274a29ceaac6a9db57f1f98012bca8ec96e9d4c3cad5fe32bc12de833a71e1d4e5
+EBUILD diffxml-0.95_beta.ebuild 1096 BLAKE2B 6f3642be1941dfdf22f90e5021b3d2c5e92fef0cbbb79c13fa1d6c0b53ec3918988a67540b79b9698b3b4cb5912df77b0fefd6b3e60ddfe7d8abd462caa12cac SHA512 ffd2e411fd79406918b1ec89aae21cf8c1c3bf47befd73ef7edcdc0aaf09fa8f0ecc5f0ee90ed00049d036635610108606515f4887de3015480a5fa7dc9ccd10
 MISC metadata.xml 213 BLAKE2B cc21376d31b4566303a0cded1f55fc6cb382cd35e08a6ba5767426d5701ff77c21cdb44333491c10c47bcf687e03b3f67fa2b8707bad14471dc5a79eacda1294 SHA512 c565d9b3b4146dcc09b42827b40cb0f8e701468431bc40601a1dee2ccf8e97a5d4f55701e11703b977d76b208b57f70d5d079e6189fe129d729d518551e595c7

dev-java/doctorj/Manifest
@@ -1,5 +1,5 @@
 DIST doctorj-5.1.2.tar.gz 1303680 BLAKE2B 5ed3745c75bb35946e2540e65fd81a999e52b0d401e176f4ab605f495ff8271a91011d85ed4b690f6351d7998ec017419f662493134e5ae83b22aeafd6c773e2 SHA512 5338be5294ed6f09961bf0ed8c1b8e7f1f367d8ef6c9d6b14520572e93b0b041829f08fac07fb2c5a1121afdadce94e329977f3c431d78112ae8c8e3967c5532
 DIST doctorj-5.2.0.tar.gz 11264957 BLAKE2B 28b9c2e9b7aabba271af11167becc5d6a1b1e1b37dda49311489a6c7150da7ffaca93fcf6afe2574234a28d37308e4483c6f9ca7f31fd2d81c403d9336247365 SHA512 0a9578490ac52330f58464721e0952494d8d13eea456b49505da493d88d1e4b3975b36c7d7865a4e48d26609e2b8776504887c68e1ab5de98fdb54897dea9008
-EBUILD doctorj-5.1.2.ebuild 788 BLAKE2B 057bb01f96cf1a025b0f70bb1e7d21f5e70ede9e772c3eb2a4d666fef8a0431381c8addacc1c1b6145a94f653ef5ff3988d06c3cb667a54b3ad568fb4f4e43b6 SHA512 2e704aeefdf17e2ba65a914a286acdd6a6b88eb974a0c28ecc431f9a363e89a868763083dfe24e79597365b71f099affb6869ca238daaf3c74b909b1d64240fa
-EBUILD doctorj-5.2.0.ebuild 965 BLAKE2B 188aaba883ee852933c1d714b9ce634ed72e1d8f642098a61e317d45c6df94e369025e81c725361eb7ef864ac618278ad30dfbff493a4126ee8b5229d1b387f9 SHA512 7ccccde964ef696988c8c0728f879a0b1cdccbfb6870240504fb6ff85470ada0b2c6dd54904784bb0dceb990a24884ae4d60885d1e4eed58b4836218b166f03e
+EBUILD doctorj-5.1.2.ebuild 777 BLAKE2B 4661c15e745b1f3aa0f7ef9e6d93ef2bf6c3f9a1564974cef54311d1acca87658de0b9863c9c89e7314fb7687557d14eebc35c20458b6ca5cb588f6c83b0ebc1 SHA512 62ba5b25cd4acb42c4d4c07a0a216e865c2d5d1b9910e439a2c6c30481cf848ebf77b423bc6acde556e8e2ac6cc11b5da03f0b55a84d7b280f081dfcf9e49ab8
+EBUILD doctorj-5.2.0.ebuild 954 BLAKE2B 71d772705a8ce757a4e9d8570207175f61986a52e290e34b72f4ddf80a24c375e3c675b1a2d2dbd5db7adbd736f01d9772a33c759629fb87d95dbb119a75ea6d SHA512 d07dae56fe4bb2574e871a307fbda27bb6622169f6250ba3f335ca823ca59fe574e93776695305f92c95622d47924beb930ded6b97822e0c7f4bd19646c8453e
 MISC metadata.xml 264 BLAKE2B a4eceb01d990f586a75362d9e435185909d183e8ec12140b50ea5f671eb2040eccfcb752bd889344072283b2792a97f6a72a96e5fd6f9d94d77c4592bfc3f36d SHA512 89245fad1dbfd8303e94eec2726150edf2132a222eabdd776693e25adfc2c1d8c61e4578ccbd8e271c965a290aa9ba10394569a0a257e827c3154bd20b607a56

dev-java/flexdock/Manifest
@@ -1,7 +1,7 @@
 AUX flexdock-1.1.1-nodemo.patch 652 BLAKE2B 869ecc773d05da7fcdff8f7831898df9795747dde40bcbd69156c836453e53e9e38266fa91f0ba8da1bce4a25f325f2ea92afb2e9ddbbafa7a2dc538f632ed79 SHA512 2f118d3b60b16664a6cc4607a7b4c57dc246a2c7c3a82ec89c8b9e7326d2cb390f00e7f46b793e2929cedd1991657fc922e946c6ad98a7f81cc4c8d1cdd993dd
 DIST flexdock-1.2.3.tar.bz2 5915009 BLAKE2B 3ec28cf023d4f6c6640bd6cf3cc27a1263df89bfcb4a3aadebf57f40f5087ab37b8bb9a3c6949a1e00842b846ec43ee962aec4fe57326a373c7f804876b6b2e7 SHA512 628a4e9b67618e96e824b21e7f89bea34e01a74d2d10a15f1a0ee2e6064fc6677b2c7ab0a7dae6a03b109370927c423c667104154b812281823e9b0d8af8dde8
 DIST flexdock-1.2.4.tar.gz 5977054 BLAKE2B f0057d4a09fa994297c437c8ecd3e0f96c922bec5f95b142f19279e154fd352628a342a8dce9dcaab4b5997665b10fc98c8daf59c766a7b83cd7adda50f2b00c SHA512 f20c92d33b0567ed1e779d4c7c843ac9d72e5ae0f0a145d69adf5066bd1dcc426cd8acfe7d0587bd36fc09cab63dd7217f27cd8eee1d5d9b522bec860df9b44f
-EBUILD flexdock-1.2.3.ebuild 1090 BLAKE2B eb446adc92cfb1de76f6d6d50ab2514e242b4ab4a1d41ff6c3b8e4f602809d075c64d8e5ee8c4080feba084b472f33a496a0ffa373914901df7b3134d3cfe92b SHA512 c7e629e0f10d68abe2099c66fc0925b100d6ca3276dcd825930516ac1ffbabce8629bd39c48cbfa7e0c079329f9c20df8517474e843de7df4777da4548385f3b
-EBUILD flexdock-1.2.4.ebuild 1067 BLAKE2B 21d41e66ca07b498d37ea589bc9ca141daee7048b2085e8e1167bfd9107f733240320a60ae2ce677e1a1e85e700023c2187646abf41d05d1b9bccaf7938aee70 SHA512 630ac13890a07f8c3b3450be0674afcdb4bc3d757feb3f4cc5701ef804b12b8b06d1145ac94d5302eef42d9321ad85fcb79e25880049513d9ec69c850f81ae9f
+EBUILD flexdock-1.2.3.ebuild 1079 BLAKE2B a36901dff2ab2623896228c77ece6559ee520ee01f330cb016b36dc66829878561c9f7c43e04e707a679cd58d7e3b4a54264e018278382dcee4aaea73f3d9b08 SHA512 33600a04f02e84d4ac67d8b4425683a217cb46ccd94f154159f615867817cb3a40ce2b88e3248b74e617d0ad98a2a179aac51822436d596756e60b3b8e1e3730
+EBUILD flexdock-1.2.4.ebuild 1056 BLAKE2B 8c9e6a979f1673755c2072ee208503e8a614da953a61307a3e1dbb9113636f7d850be91881adb4c6757cb3d5aebf6014fef4dbeb1a348c22ab40935bc112b570 SHA512 53cd7cda8fddd1ddccaab4874276f85b6c134a8784953e7b872af4d763d7a77e0c80bd7bc43760aef6ba34d9908cde3184717d154eae8e5c9e5a906e7a9a84aa
 MISC ChangeLog 1444 BLAKE2B 059811c07b5091d193b99e6005e6def0a01a918598f071a9768461c2b73285f1eb711bbdf76f9bdea1a39797a0b769cd0495afe6f3f9b8d176909c2ff4a96538 SHA512 5c3af990e5ab2d3ee32612df5ca97548f3bf58c8bbf5258670a9e41ab18848f93bde273e12f1f4c57fa213b965b995bd40ef393ea93864adf0233a1a23c7e8c8
 MISC metadata.xml 222 BLAKE2B 1a612395e1de8830065210a6dcd6d00e30ca7546118f97c88aed9718439d9ade7ebe4947d1444df9c09d8fb4d19b84880791bc44cf6066ee60eb8dc3ff6a7e0f SHA512 30f86d0aec0d8e5f174a7cd8e9a96fbe25e076542e8886967071067bd5c85730f8297d112ebef0b098bf4be3f17c6cc071f577b9517e15a88301eaf962c40ea1

dev-java/jaranalyzer/Manifest
@@ -1,4 +1,4 @@
 AUX build.xml 1910 BLAKE2B 3c61e7c36354dd383b03e3726b15740ef6c802abb490443f90793a8842234a3c4ed5c17b452af0df27bddf3cbd83b78eb6651df60b8f04a60d5c4906efd126d0 SHA512 7577d70e497e6154929b57617462de35c82795638f2978fb2ab0d7c81cc7b38c681bc4cf2eabc72c1e9be9cc393f42d977424fdf909027dbafa93fb1dc7a57cf
 DIST JarAnalyzer-src-1.2.1.zip 658085 BLAKE2B 112ffe8dd221587aa6470263008b6bcab8aedd305c730d5545fee0de38503341f650927ebe65439a025f49ba31e2c4f73c36669dc579addb23d3c8cdccd16efd SHA512 2e0ee278eefc1f43111707d938fceb4200e2d9e5942677d4fd4ad3a5c1ca78ddd1e4f80e7a2405955c2bba99a36e882409b8aad4ca68f0c0a9e7b713b720578c
-EBUILD jaranalyzer-1.2.1-r2.ebuild 1536 BLAKE2B 444f54e57094c4704c7027406676230fc41b3c3575fbc1940953f06f3c0fe3daac578b7259f69d154d0378026e4ae6ef02f7f196baa5e8908769fdc8dc376285 SHA512 c569759c0ef8a886d40dca41ea234b80dd2f35648d21b472a978d092d2ca0e8f9196c972157ece0f9886cd60b60b13dc5e485874ef7382980e691048afcc180d
+EBUILD jaranalyzer-1.2.1-r2.ebuild 1525 BLAKE2B 5b416e8252756238009dd4d104cfbea8763a4d6c37f95fb0cfccc76d1ed5819ea786a1cc24287d93a970e189ce785da91cbf312b58fadce8d0287b1234e19561 SHA512 bb473758a963ffafdd4349c8b3222ac59da63eca1355ff4f04f4120133ca741ddaa325651580f1032b4f59d91ae0a1dc20e5d67e331c3636e642b884eb65398c
 MISC metadata.xml 140 BLAKE2B b079a1406c8131151cb09483d0bedc47488264a4fce263e413bafaf54ef7aef23d0173e17847908732fb42bf4e704a128050651f020f10e4cf2f44197874276d SHA512 7e8069b4a2eaf234ea437e95578e2fd6e007038bf98df9f508adfa3f5af0e746993074a91ada2517c520bfda84d193b33c7a41f184b78bf128aa51b5c0c76e40

commit a4d596b051cdfd7eec50bea67fc6bca8074d9e9d

app-misc/mapcraft: Fix build
We relied on behavior of java-ant-2.eclass, so this ebuild
was broken by 4644ad639af89b7c22af52b27feb32f553fc68df.

app-misc/mapcraft/Manifest
@@ -1,5 +1,6 @@
+AUX eant.patch 419 BLAKE2B d67c3a241fbe66ff4821f80e1984db2ee17f90c7e051568a065ec1bf961b5afbefb0d0da27b67d742266ad749cb2a4db87ae1d2b95a355fd6d8dcf9b29df9e04 SHA512 67e69e16c51f49bdc7f2379309fa4615527c87a80e8bea51c31befcab026430b888e0b8e04df708ef0676524c8a310b1de5afc48369d9e5c3e4e1bd173981ae6
 AUX incompatible-type.patch 707 BLAKE2B ed54793876bb9a13f8771d3eda49edf482fb84605515e31a161c51d0667e3987455642754e42ffe8ce988438753a274fc1a35013c650efc3fd8ae009a22906be SHA512 8b759a08c623e2ff885bb8bfc58281e8bbcf4a8a576ddda3efe71a04f013d46e5a7f2e77ce28ec6c3e3e826450635cb289d8c29f619c2a7e5d141338b2d211ae
 AUX internal-api.patch 781 BLAKE2B 8823b83ed42db84a628488f42529db9a253f9867c5bb171e209048f8d423d2201941204f976060f3f1e89aa31b0fc5e1b5a6a9504bbcde0af0352fc6138fe1ea SHA512 378bf065f198ea642f6f8254bcb02f70a2722732ca815870888f82caf0abc4efd1ad695c99ebc7ca7d4f7389aa393c191d851f762632c14654810f2d8b4e0b8e
 DIST mapcraft-0.3b.tgz 1838057 BLAKE2B f914cff40fa4182c5360b5f001784b1274ee198bf51dc1ae53dfd78eed21dbc7cb3b58548f2f5baf38e60c7614d3d962cb3d4a637ff9c6ae098bf8c778b54368 SHA512 edafa75b84c46b5384add84bef5ece62b1a98f234183cec2073919821b7d96c458c99027bef8b22ccb76ebd35d74ccd5f3d0367ac1ae8844968a6e62723e7eb8
-EBUILD mapcraft-0.3b.ebuild 1097 BLAKE2B e3f41e5acf9c56f4477c6c8301ff93edb9b57a1aa5fabbd84f5c1779706ebb4d3e7c1225edfaf66748ff69acd5f40de6a4f38fefd2a934d9877970afb8ce25ec SHA512 4e0e04cb4222f641f42e83a45bc93fbbffb8d05cf5beaebf62350346deff5d11899e7e5c31e751718a1328a9e5b4d6f63ecfbe9adcca6ebd9f1cc048a9950ab2
+EBUILD mapcraft-0.3b.ebuild 1179 BLAKE2B 272c9e3cb421bb1cb4ebd371f26f261d2f00e88091b225ecf0a468618c48213d29129bf75031064dd723b4d9d2bbf131292be17a6b6d39ef8654d1ef532c611e SHA512 77889fa8f70ffa51dc8db808a10bf2ac2e51be1ea5634080251f06432efc98c3524e9c10072f8b90336e128c02956df9b5a7bd06f07ec83ad715f4450be1a304
 MISC metadata.xml 214 BLAKE2B fc4a40d42b9ac5bc66bdcc886771d647cb3b8c88ff4e1d2f51bf40e261e9b9b8061b5c7527160007e63447bbed8a0a190b75cdd76a18341de5281ed0e4a1dbf0 SHA512 1b71cae306f6cbf69abe3051974d05050da9470c2b3b3655ebd1dd249091590fe10d6ca2df8f0e7c772671b57aa36c836cb9d86af069c250b43b8d1feea3e210

app-misc/mapcraft/files/eant.patch
@@ -1,0 +1,13 @@
+diff '--color=auto' -ru a/build.xml b/build.xml
+--- a/build.xml	2004-06-11 15:10:30.000000000 -0400
++++ b/build.xml	2025-02-01 11:28:46.137210425 -0500
+@@ -29,6 +29,9 @@
+         </copy>
+         <javac srcdir="${src.dir}" destdir="${build.dir}" debug="on">
+             <classpath refid="lib.path"/>
++	    <classpath>
++		<pathelement path="${gentoo.classpath}" />
++	    </classpath>
+         </javac>
+     </target>
+ 

app-misc/mapcraft/mapcraft-0.3b.ebuild
@@ -24,9 +24,15 @@ DEPEND="${COMMON_DEPEND}"
 IUSE="doc source"
 
 PATCHES=(
+	"${FILESDIR}/eant.patch"
 	"${FILESDIR}/internal-api.patch"
 	"${FILESDIR}/incompatible-type.patch"
 )
+
+src_prepare() {
+	eapply "${PATCHES[@]}"
+	eapply_user
+}
 
 src_compile() {
 	EANT_GENTOO_CLASSPATH="xerces-2 xalan junit" eant